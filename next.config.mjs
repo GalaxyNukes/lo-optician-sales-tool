aa323c25fa2a2fc6819a4b6d8c8daf007838ccc5
GalaxyNukes <jensdemeyer18@gmail.com>
2026-04-13 17:43:00 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,6 @@ const nextConfig = {
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-Frame-Options', value: 'DENY' },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
         ],
       },
     ]
